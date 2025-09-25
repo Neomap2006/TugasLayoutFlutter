49f0e6125891502e18ee1060be6e2ffd044a85c5
Neo Map <neomap4441v@gmail.com>
2025-09-26 05:01:01 +0700

    mulai dari nol

--- lib/tugas/Quiz.dart
+++ lib/tugas/Quiz.dart
@@ -6,37 +6,32 @@ class Layout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Training",
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 50),
-            child: Row(
-              children: const [
-                Icon(Icons.chevron_left, size: 22),
-                SizedBox(width: 4),
-                Icon(Icons.calendar_month, size: 20),
-                SizedBox(width: 4),
-                Icon(Icons.chevron_right, size: 22),
-              ],
-            ),
-          ),
-        ],
-      ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Bagian judul atas
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
+                children: const [
+                  Text(
+                    "Training",
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(width: 150),
+                  Icon(Icons.chevron_left, size: 22),
+                  Icon(Icons.calendar_month, size: 20),
+                  Icon(Icons.chevron_right, size: 22),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: const [
+                  Text(
                     "Your Program",
                     style: TextStyle(
                       fontSize: 20,
@@ -44,79 +39,18 @@ class Layout extends StatelessWidget {
                       color: Colors.black,
                     ),
                   ),
-                  Row(
-                    children: const [
-                      Text(
-                        "Details",
-                        style: TextStyle(fontSize: 16, color: Colors.blue),
-                      ),
-                      SizedBox(width: 4),
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        size: 14,
-                        color: Colors.blue,
-                      ),
-                    ],
+                  SizedBox(width: 80),
+                  Text(
+                    "Details",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
                   ),
+                  SizedBox(width: 4),
+                  Icon(Icons.arrow_forward_ios, size: 14, color: Colors.blue),
                 ],
-              ),
-              const SizedBox(height: 25),
-              // Card Gradient
-              Container(
-                height: 200,
-                width: 450,
-                padding: const EdgeInsets.all(18),
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(10),
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(120),
-                  ),
-                  gradient: const LinearGradient(
-                    colors: [
-                      Color.fromARGB(255, 177, 12, 189),
-                      Color.fromARGB(255, 198, 98, 220),
-                    ],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Next Workout",
-                      style: TextStyle(color: Colors.white, fontSize: 13),
-                    ),
-                    const Text(
-                      "Lest Toning",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const Text(
-                      "and Glutes Workout",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const Spacer(),
-                    Row(
-                      children: const [
-                        Icon(Icons.timer, color: Colors.white, size: 18),
-                        SizedBox(width: 6),
-                        Text(
-                          "60 min",
-                          style: TextStyle(color: Colors.white, fontSize: 16),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
               ),
             ],
           ),
