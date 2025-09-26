86d344d2ced953d60ae304441a5f9e86ed376394
Neo Map <neomap4441v@gmail.com>
2025-09-26 21:25:14 +0700

    Update setelah 15 menit

--- lib/tugas/Quiz.dart
+++ lib/tugas/Quiz.dart
@@ -12,7 +12,9 @@ class Layout extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              // Header
               Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: const [
                   Text(
                     "Training",
@@ -22,7 +24,7 @@ class Layout extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  Spacer(),
+                  SizedBox(width: 8),
                   Icon(Icons.chevron_left, size: 22),
                   SizedBox(width: 1),
                   Icon(Icons.calendar_month, size: 20),
@@ -41,7 +43,7 @@ class Layout extends StatelessWidget {
                       color: Colors.black,
                     ),
                   ),
-                  Spacer(),
+                  SizedBox(width: 8),
                   Text(
                     "Details",
                     style: TextStyle(
@@ -55,81 +57,89 @@ class Layout extends StatelessWidget {
                 ],
               ),
               const SizedBox(height: 16),
-              Container(
-                height: 150,
-                width: 450,
-                padding: const EdgeInsets.all(18),
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(18),
-                    topLeft: Radius.circular(18),
-                    topRight: Radius.circular(100),
-                    bottomRight: Radius.circular(18),
-                  ),
-                  gradient: const LinearGradient(
-                    colors: [
-                      Color.fromARGB(255, 189, 12, 142),
-                      Color.fromARGB(255, 232, 106, 211),
-                    ],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Next Workout",
-                      style: TextStyle(color: Colors.white, fontSize: 13),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  height: 150,
+                  width: 450,
+                  padding: const EdgeInsets.all(18),
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(18),
+                      topLeft: Radius.circular(18),
+                      topRight: Radius.circular(100),
+                      bottomRight: Radius.circular(18),
                     ),
-                    const SizedBox(height: 1),
-                    const Text(
-                      "Leg Toning",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const Text(
-                      "and Glutes Workout",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const Spacer(),
-                    Row(
-                      children: [
-                        const Icon(Icons.timer, color: Colors.white, size: 15),
-                        const SizedBox(width: 5),
-                        const Text(
-                          "60 min",
-                          style: TextStyle(color: Colors.white, fontSize: 14),
-                        ),
-                        const Spacer(),
-                        Container(
-                          decoration: const BoxDecoration(
-                            color: Colors.white,
-                            shape: BoxShape.circle,
-                          ),
-                          padding: const EdgeInsets.all(6),
-                          child: const Icon(
-                            Icons.play_arrow,
-                            color: Color.fromARGB(255, 177, 12, 189),
-                            size: 20,
-                          ),
-                        ),
+                    gradient: const LinearGradient(
+                      colors: [
+                        Color.fromARGB(255, 189, 12, 142),
+                        Color.fromARGB(255, 232, 106, 211),
                       ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
                     ),
-                  ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Next Workout",
+                        style: TextStyle(color: Colors.white, fontSize: 13),
+                      ),
+                      const SizedBox(height: 1),
+                      const Text(
+                        "Leg Toning",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Text(
+                        "and Glutes Workout",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Spacer(),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.timer,
+                            color: Colors.white,
+                            size: 15,
+                          ),
+                          const SizedBox(width: 5),
+                          const Text(
+                            "60 min",
+                            style: TextStyle(color: Colors.white, fontSize: 14),
+                          ),
+                          const Spacer(),
+                          Container(
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.circle,
+                            ),
+                            padding: const EdgeInsets.all(6),
+                            child: const Icon(
+                              Icons.play_arrow,
+                              color: Color.fromARGB(255, 177, 12, 189),
+                              size: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              const SizedBox(height: 30),
+              const SizedBox(height: 40),
               Stack(
                 clipBehavior: Clip.none,
                 children: [
+                  // Card Background
                   Container(
                     height: 100,
                     width: 450,
@@ -139,13 +149,6 @@ class Layout extends StatelessWidget {
                         image: AssetImage("Gambar/card.jpg"),
                         fit: BoxFit.cover,
                       ),
-                    ),
-                  ),
-                  Container(
-                    height: 150,
-                    width: 450,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(18),
                     ),
                   ),
                   Positioned(
@@ -186,6 +189,53 @@ class Layout extends StatelessWidget {
                             fontWeight: FontWeight.bold,
                             fontSize: 14,
                           ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    top: 120,
+                    left: 0,
+                    right: 0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Area of Focus",
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87,
+                          ),
+                        ),
+                        const SizedBox(height: 12),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 120,
+                              width: 150,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(18),
+                                image: const DecorationImage(
+                                  image: AssetImage("Gambar/ex3.png"),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 25),
+                            Container(
+                              height: 120,
+                              width: 150,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(18),
+                                image: const DecorationImage(
+                                  image: AssetImage("Gambar/ex4.png"),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
